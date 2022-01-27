84f142efba50805f9bee28db772aab9563d9e787
schoegel <98523491+schoegel@users.noreply.github.com>
2022-01-27 14:02:29 +0100

    Update Projet oscillant.py

--- Projet oscillant.py
+++ Projet oscillant.py
@@ -1,13 +1,4 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-
-from scipy.integrate import ode
-import numpy as np
-import matplotlib.pylab as plt
-
-
-
 #variables
 
 f0 = 1000 #s-1
@@ -16,13 +7,36 @@ q0 = 100
 
 k0 = 20 #N/m
 
-w0 = 2pif0 = sqrt(k0/m0)
+#w0 = 2pif0 = sqrt(k0/m0)
 
-q0 = m0*w0/gam0
+#q0 = m0*w0/gam0
 
-fs = 40*f0 = 40000 #s-1
+fs = 40*f0 
+
+Ts = 1/fs #s
 
 Tw = 10 #s
 
-N = Tw/Ts = 400000
+#N = Tw/Ts = 400000
+
+df0 = 30 #s-1
+
+
+#trace temporel de fréquence
+
+tab_t = np.arange(0,10,Ts) 
+
+print(tab_t)
+
+tab_f = np.arange(0,2000,Ts)
+
+print(tab_f)
+
+plt.plot(tab_x_rungekuttascipy,tab_y_rungekuttascipy, label = "rungekutta scipy" )      
+plt.legend()
+plt.xlabel("x")
+plt.ylabel("y(x)") 
+plt.title("rungekuttascipy")
+plt.savefig("rungekuttascipy.pdf")       
+plt.show()
 
