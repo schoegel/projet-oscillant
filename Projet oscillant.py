6f97790a97a5a3644db75e575f2d9b79f6cadc93
schoegel <98523491+schoegel@users.noreply.github.com>
2022-01-27 11:24:26 +0100

    Add files via upload

---
+++ Projet oscillant.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+from scipy.integrate import ode
+import numpy as np
+import matplotlib.pylab as plt
+
+
+
+#variables
+
+f0 = 1000 #s-1
+
+q0 = 100 
+
+k0 = 20 #N/m
+
+w0 = 2pif0 = sqrt(k0/m0)
+
+q0 = m0*w0/gam0
+
+fs = 40*f0 = 40000 #s-1
+
+Tw = 10 #s
+
+N = Tw/Ts = 400000
+
